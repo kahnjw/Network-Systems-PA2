2e92f6d0a4c1bb63f7a2ad55c8468a92c3a38b7e
kahnjw <jarrod.kahn@gmail.com>
2012-10-03 17:37:51 -0600

    Ninth commit

--- SRPclient.c
+++ SRPclient.c
@@ -15,6 +15,8 @@
 #include <unistd.h>
 #include "sendto_.h"
 #include <errno.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include "types.h"
 
@@ -43,6 +45,13 @@ int main(int argc, char *argv[]) {
   char directemp[BUFSIZE];
   char filename[BUFSIZE];
   char msgbuffer[512];
+  struct timeval time;
+
+  struct timeval timeVal;
+  fd_set readFDS;
+  int iSockRet, iSelRet;
+
+  
 
     //check command line args.
     if(argc<6) {
@@ -85,7 +94,7 @@ int main(int argc, char *argv[]) {
     strcpy(msgbuffer,filebuffer);
 
     printf("error rate : %f\n",atof(argv[2]));
-
+    printf("%s: sending data to '%s' \n", argv[0], argv[1]);
     
 
     /* Note: you must initialize the network library first before calling
@@ -94,8 +103,9 @@ int main(int argc, char *argv[]) {
 
     /* get server IP address (input must be IP address, not DNS name) */
 
-    printf("%s: sending data to '%s' \n", argv[0], argv[1]);
 
+    //setup socket structs
+    /***************************************************/
     remoteServAddr.sin_family = AF_INET;
     remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);
     inet_pton(AF_INET, argv[1], &remoteServAddr.sin_addr);
@@ -103,6 +113,9 @@ int main(int argc, char *argv[]) {
     cliAddr.sin_family = AF_INET;
     cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     cliAddr.sin_port = htons(0);
+    /*************************************************/
+
+
 
     /* socket creation */
     if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -110,31 +123,57 @@ int main(int argc, char *argv[]) {
       exit(1);
     }
 
-    //char msg[] = "This is a test";  
+    
+
+
     setFrame(&framearray[0],0,0,sizeof(msgbuffer),filename,msgbuffer);
     
     printFrame(framearray[0]);
     
     sendFrame = makedatapacket(framearray[0]);
 
- 
-    if((sendto_(sock,sendFrame, strlen(sendFrame),0, (struct sockaddr *) &remoteServAddr,
+    while(1){
+      gettimeofday(&time,NULL);
+      double t1 = time.tv_sec + (time.tv_usec/1000000.0);
+      printf("Sent time: %f\n", t1);
+      if((sendto_(sock,sendFrame, strlen(sendFrame),0, (struct sockaddr *) &remoteServAddr,
 		sizeof(remoteServAddr)))< 0 ){
-      printf("Error Sending\n");
-      perror("sendto()");
-      exit(1);
+	printf("Error Sending\n");
+	perror("sendto()");
+	exit(1);
+      }
+      
+      timeVal.tv_sec = 10;
+      timeVal.tv_usec = 0;
+
+      FD_ZERO(&readFDS);
+      FD_SET(sock, &readFDS);  
+      
+      iSelRet = select(0, &readFDS, NULL, NULL, &timeVal);
+
+      if ((iSelRet != -1) && (iSelRet != 0)){
+    
+	if(recvfrom(sock, &recvmsg, sizeof (recvmsg), 0,(struct sockaddr *) &recAddr, &recLen) < 0){
+	  perror("recvfrom()");
+	  exit(1);
+	}
+
+	printf("Received Ack!\n");
+      
+	printf("Ack Buffer: %s\n", recvmsg);
+
+      }
+      else if (iSelRet == 0) {
+	printf("timeout\n");
+	gettimeofday(&time,NULL);
+	double t2= time.tv_sec + (time.tv_usec/1000000.0);
+	printf("Time Out Time time: %f\n",t2);
+      } else if (iSelRet < 0) {
+	printf("select() failed\n");
+      }
     }
 
     free(sendFrame);
-   
-    
-    if(recvfrom(sock, &recvmsg, sizeof (recvmsg), 0,(struct sockaddr *) &recAddr, &recLen) < 0){
-      perror("recvfrom()");
-      exit(1);
-    }
-
-    printf("Received Ack!\n");
-    printf("Ack Buffer: %s\n", recvmsg);
     
     
 }
